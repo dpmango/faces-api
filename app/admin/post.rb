c403d4780fbc8513082104dccca9459695ef4b5a
Sergey Khmelevskoy <xs290@me.com>
2017-10-16 00:20:46 +0300

    preview images

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -12,12 +12,25 @@ ActiveAdmin.register Post do
   #   permitted
   # end
 
+  index do |post|
+    selectable_column
+    column 'Image' do |firmware|
+      link_to image_tag(firmware.photo.url,width:100), edit_admin_post_path(firmware.id)
+    end
+    # id_column
+    column 'Name', :name
+    column :category
+    column :filter
+    column :created_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :name
       f.input :description, :as => :ckeditor
       f.input :category
-      f.input :photo, :as => :file
+      f.input :photo, :as => :file, :hint => image_tag(f.object.photo.url,width:100)
       f.input :filter
     end
     f.actions
