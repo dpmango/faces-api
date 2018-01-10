8c246493ef3934629b1dfa287ce381ef23e07104
dpmango <xs290@me.com>
2018-01-10 19:17:38 +0200

    added strip whitespace

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 class Post < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  before_validation :strip_whitespace
 
   # mount_uploader :photo, PostUploader
   mount_base64_uploader :photo, PostUploader
+
+  private
+  def strip_whitespace
+    self.category = self.category.strip unless self.category.nil?
+    # self.email = self.email.strip unless self.email.nil?
+    # self.nick = self.nick.strip unless self.nick.nil?
+  end
+
 end
